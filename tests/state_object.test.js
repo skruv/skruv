df9ed570b15ec70512c30aec3265aded362f82f1
Svante Richter <svante.richter@gmail.com>
2021-03-29 13:45:52 +0200

    Add more testing

---
+++ tests/state_object.test.js
@@ -0,0 +1,25 @@
+/* global test expect */
+import { renderNode } from '../vDOM.js'
+import { createState } from '../state.js'
+import { body, div } from '../html.js'
+
+const wait = time => new Promise(resolve => setTimeout(resolve, time))
+
+test('update on array push', async () => {
+  const sub = createState({ obj: {} })
+  const html = document.createElement('html')
+  let root = document.createElement('body')
+  html.appendChild(root)
+  ;(async () => {
+    for await (const state of sub) {
+      root = renderNode(body({}, Object.keys(state.obj).map(key => div({ 'data-key': key }, state.obj[key]))), root)
+    }
+  })()
+  sub.obj.test = 'testvalue'
+  sub.obj.test2 = 'testvalue2'
+  await wait(20)
+  expect(html.childNodes[0].childNodes[0].textContent).toBe('testvalue')
+  expect(html.childNodes[0].childNodes[0].getAttribute('data-key')).toBe('test')
+  expect(html.childNodes[0].childNodes[1].textContent).toBe('testvalue2')
+  expect(html.childNodes[0].childNodes[1].getAttribute('data-key')).toBe('test2')
+})
