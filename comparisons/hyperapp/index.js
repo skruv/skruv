19e90b0b97bb7f9f51b94ca835c01e21f46b1cb7
Svante Richter <svante.richter@gmail.com>
2022-07-09 19:42:05 +0200

    Move entrypoint, check examples

---
+++ comparisons/hyperapp/index.js
@@ -0,0 +1,6 @@
+
+import * as elements from '@hyperapp/html'
+import * as svgElements from '@hyperapp/svg'
+import { app, h, text } from 'hyperapp'
+
+export { app, elements, h, svgElements, text }
---
+++ types/skruv.d.ts
@@ -0,0 +1,5 @@
+import createState from "./createState.js";
+import * as elements from "./elements.js";
+import render from "./render.js";
+export { createState, elements, render };
+//# sourceMappingURL=skruv.d.ts.map
