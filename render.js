d28a00eaa1645402354951f73497bfcf3addd027
Svante Richter <svante.richter@gmail.com>
2022-07-09 12:58:26 +0200

    Trying more stuff to fix generator typings

--- render.js
+++ render.js
@@ -22,7 +22,7 @@
  * @property {ChildNodes} [result]
  * @property {Boolean} [booted]
  *
- * @typedef {(AsyncGenerator<any, ChildNodes> | AsyncIterable<ChildNodes>) & SkruvAdditionalIterableProperties} SkruvIterableType
+ * @typedef {(AsyncGenerator<ChildNodes> | AsyncIterable<ChildNodes>) & SkruvAdditionalIterableProperties} SkruvIterableType
  */
 
 /**
