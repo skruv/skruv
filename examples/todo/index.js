7033eb285af0c5527e5c5c20e149fdbd10e0d1c3
Svante Richter <svante.richter@gmail.com>
2023-06-11 17:18:37 +0200

    Remove custom attributes on DOM elements

--- examples/todo/index.js
+++ examples/todo/index.js
@@ -25,47 +25,33 @@ input {
 `
 
 render(
-  h('html', { lang: 'en-US' },
-    h('head', {},
-      h('title', {}, state.todos.getGenerator(0)),
-      h('script', { src: './index.js', type: 'module' }),
-      h('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }),
-      styles
-    ),
-    h('body', {},
-      h('main', {},
-        h('h1', {}, state.todos.getGenerator(0)),
-        h('form',
-          {
-            onsubmit: e => {
-              e.preventDefault()
-              state.todos.unshift(new FormData(e.target).get('todo'))
-              e.target.reset()
-            }
-          },
-          h('input', {
-            type: 'text',
-            name: 'todo'
-          }),
-          h('button', {}, 'New!')
-        ),
-        async function* () {
-          for await (const currentState of state) {
-            yield h('ol', {},
-              currentState.todos.map((todo, i) => h('li', {},
-                todo,
-                ' ',
-                h('a', {
-                  href: '#',
-                  onclick: () => {
-                    currentState.todos.splice(i, 1)
-                  }
-                }, 'x')
-              ))
-            )
-          }
+  h('html', { lang: 'en-US' }, h('head', {}, h('title', {}, state.todos.getGenerator(0)), h('script', { src: './index.js', type: 'module' }), h('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }), styles
+  ), h('body', {}, h('main', {}, h('h1', {}, state.todos.getGenerator(0)), h('form',
+    {
+      onsubmit: e => {
+        e.preventDefault()
+        state.todos.unshift(new FormData(e.target).get('todo'))
+        e.target.reset()
+      }
+    },
+    h('input', {
+      type: 'text',
+      name: 'todo'
+    }),
+    h('button', {}, 'New!')
+  ), async function * () {
+    for await (const currentState of state) {
+      yield h('ol', {}, currentState.todos.map((todo, i) => h('li', {}, todo, ' ', h('a', {
+        href: '#',
+        onclick: () => {
+          currentState.todos.splice(i, 1)
         }
+      }, 'x')
+      ))
       )
-    )
+    }
+  }
+  )
+  )
   )
 )
