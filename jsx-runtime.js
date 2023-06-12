7eb67020690067b17e7c88b9aa9e2f1051e2830f
Svante Richter <svante.richter@gmail.com>
2023-06-12 11:22:36 +0200

    Linting

--- jsx-runtime.js
+++ jsx-runtime.js
@@ -1,22 +1,25 @@
-const kebabize = (str) => str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? "-" : "") + $.toLowerCase())
+const kebabize = str => str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? '-' : '') + $.toLowerCase())
 
+// eslint-disable-next-line import/no-unused-modules
 export const Fragment = '#fragment'
 
-export const jsx = (nodeName, attributes = {}) => jsxs(nodeName, {...attributes, children: [attributes.children]})
+// eslint-disable-next-line import/no-unused-modules
+export const jsx = (nodeName, attributes = {}) => jsxs(nodeName, { ...attributes, children: [attributes.children] })
 
+// eslint-disable-next-line import/no-unused-modules
 export const jsxs = (nodeName, attributes = {}) => {
-  if (nodeName === Fragment) return attributes.children
-  let { children, ...attrs } = attributes
+  if (nodeName === Fragment) { return attributes.children }
+  const { children, ...attrs } = attributes
   Object.keys(attrs).forEach(e => {
     attrs[kebabize(e)] = attrs[e]
     delete attrs[e]
   })
   if (attrs['class-name']) {
-    attrs['class'] = attrs['class-name']
+    attrs.class = attrs['class-name']
     delete attrs['class-name']
   }
   if (attrs['html-for']) {
-    attrs['for'] = attrs['html-for']
+    attrs.for = attrs['html-for']
     delete attrs['html-for']
   }
   return ({ nodeName, attributes: attrs || {}, childNodes: children })
