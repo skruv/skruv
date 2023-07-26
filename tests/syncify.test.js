4953a2510a8d2f7e26c1d4850bdd09dbc7bd110f
Svante Richter <svante.richter@gmail.com>
2023-07-26 18:32:52 +0200

    More syncify testing

--- tests/syncify.test.js
+++ tests/syncify.test.js
@@ -3,8 +3,8 @@ import test from 'node:test'
 
 import { hydrationPromise, syncify } from '../utils/syncify.js'
 
-const internalFunc1 = () => {}
-const internalFunc2 = () => {}
+const internalFunc1 = () => { }
+const internalFunc2 = () => { }
 
 const a = {
   a: new Promise(resolve => resolve('a')),
@@ -12,10 +12,19 @@ const a = {
     await new Promise(resolve => setTimeout(() => resolve(''), 5))
     yield 'b'
   },
+  ai: (async function * () {
+    await new Promise(resolve => setTimeout(() => resolve(''), 10))
+    yield 'ai'
+  })(),
   d: [
     () => [
       async () => [
         async function * () {
+          yield {
+            a: {
+              'data-skruv-finished': false
+            }
+          }
           await new Promise(resolve => setTimeout(() => resolve(''), 10))
           yield {
             a: new Promise(resolve => setTimeout(() => resolve('a'), 10))
@@ -24,6 +33,7 @@ const a = {
       ]
     ]
   ],
+  func: async () => 'func',
   _a: internalFunc1,
   onclick: internalFunc2,
   c: 'c',
@@ -45,19 +55,23 @@ test('syncify', async () => {
   assert.strictEqual(syncify(a).c, 'c')
   assert.strictEqual(syncify(a).f, false)
   assert.strictEqual(syncify(a).n, null)
+  assert.strictEqual(syncify(a).func, 'func')
   assert.strictEqual(syncify(a)._a, internalFunc1)
   assert.strictEqual(syncify(a).onclick, internalFunc2)
   assert.strictEqual(syncify(a).b, 'b')
+  assert.strictEqual(syncify(a).ai, 'ai')
   assert.strictEqual(syncify(a).d[0][0][0].a, 'a')
   assert.throws(() => syncify({ s: Symbol.for('s') }))
-  const newObj = { i: async function * () {
-    let i = 0
-    yield i
-    while (true) {
-      yield i++
+  const newObj = {
+    i: async function * () {
+      let i = 0
+      yield i
+      while (true) {
+        yield i++
+      }
     }
-  } }
+  }
   syncify(newObj)._r._r = () => false
   await new Promise(resolve => setTimeout(() => resolve(''), 1))
-  assert.strictEqual(syncify(newObj).i, '0')
+  assert.strictEqual(syncify(newObj).i, 0)
 })
