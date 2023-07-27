bd31e60404adb92eb63ac0868c0e7c9bd0a61a21
Svante Richter <svante.richter@gmail.com>
2023-07-27 21:28:55 +0200

    Update main example to use build and jsx

---
+++ examples/todo-no-state-jsx/index.js
@@ -0,0 +1,66 @@
+import { htmlFactory, render } from '../../index.js'
+
+const { html, head, title, meta, body, main, h1, form, input, button, ol, li, a, style } = htmlFactory
+
+const state = {
+  todos: ['Write todos']
+}
+
+const styles = /* css */`
+  :scope {
+    color: #f1f1f1;
+    background: #0f0f0f;
+  }
+
+  body {
+    max-width: 40ch;
+    margin: 0 auto;
+  }
+
+  form { display: flex; }
+  input { flex: 1; }
+  a { color: #9b9b9b; }
+`
+
+const doRender = () => render(
+  html({ lang: 'en-US' },
+    head(
+      title(state.todos[0]),
+      meta({ name: 'viewport', content: 'width=device-width, initial-scale=1' }),
+      style(styles)
+    ),
+    body(
+      main(
+        h1(state.todos[0]),
+        form(
+          {
+            onsubmit: e => {
+              e.preventDefault()
+              state.todos.unshift(new FormData(e.target).get('todo'))
+              e.target.reset()
+              doRender()
+            }
+          },
+          input({
+            name: 'todo'
+          }),
+          button('New!')
+        ),
+        ol(
+          state.todos.map((todo, i) => li(
+            `${todo} `,
+            a({
+              href: '#',
+              onclick: e => {
+                e.preventDefault()
+                state.todos.splice(i, 1)
+                doRender()
+              }
+            }, 'x')
+          ))
+        )
+      )
+    )
+  )
+)
+doRender()
---
+++ examples/todo/index.jsx
@@ -0,0 +1,66 @@
+import { render } from 'skruv'
+import { css, cssTextGenerator } from 'skruv/utils/css.js'
+import { createState } from 'skruv/utils/state.js'
+
+const state = createState({ todos: ['Write todos'] })
+
+const styles = css`
+  :scope {
+    color: #f1f1f1;
+    background: #0f0f0f;
+  }
+
+  body {
+    max-width: 40ch;
+    margin: 0 auto;
+  }
+
+  form { display: flex; }
+  input { flex: 1; }
+  a { color: #9b9b9b; }
+`
+
+render(
+  <html lang="en-US" class={styles}>
+    <head>
+      <title>{state.todos.getGenerator(0)}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <style>{cssTextGenerator}</style>
+    </head>
+    <body>
+      <main>
+        <h1>{state.todos.getGenerator(0)}</h1>
+        <form onsubmit={
+          e => {
+            e.preventDefault()
+            state.todos.unshift(new FormData(e.target).get('todo'))
+            e.target.reset()
+          }
+        }>
+          <input name="todo"></input>
+          <button>New!</button>
+        </form>
+        {async function * () {
+          for await (const currentState of state) {
+            yield (
+              <ol>
+                {currentState.todos.map((todo, i) => (
+                  <li>{todo} <a
+                    href="#"
+                    onclick={
+                      e => {
+                        e.preventDefault()
+                        currentState.todos.splice(i, 1)
+                      }
+                    }
+                  >x</a>
+                  </li>
+                ))}
+              </ol>
+            )
+          }
+        }}
+      </main>
+    </body>
+  </html>
+)
