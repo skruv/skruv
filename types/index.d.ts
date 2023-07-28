e4e2c404e94bdf2f7781a900a1da32db305bf497
Svante Richter <svante.richter@gmail.com>
2023-07-29 00:37:31 +0200

    Minor fixes

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,9 @@
+export function h(t: string, ...c: (Record<string, any> | Vnode)[]): Vnode;
+export function render(current: Vnode, _currentNode?: Node, parentNode?: ParentNode | null, isSvg?: any): boolean;
+export const elementFactory: any;
+export type SkruvChildNode = Vnode | Vnode[] | string | boolean | number;
+export type SkruvChildNodes = SkruvChildNode[];
+export type VnodeAtrributes = Record<string, (string | boolean | Function | number | Object)>;
 export type Vnode = {
     s: Symbol;
     t: string;
@@ -11,23 +17,4 @@ export type Vnode = {
         _r: () => boolean;
     } | undefined;
 };
-/**
- * @typedef {Vnode|Vnode[]|String|Boolean|Number} SkruvChildNode
- * @typedef {SkruvChildNode[]} SkruvChildNodes
- */
-/**
- * @typedef {object} Vnode
- * @prop {Symbol} s
- * @prop {String} t
- * @prop {SkruvChildNodes} c
- * @prop {Record<string,(string|boolean|Function|number|Object)> & {_r:{_r:() => boolean}?}} a
- * @prop {{_r:() => boolean}} [_r]
- */
-/** @type {Vnode} */
-export const Vnode: Vnode;
-export function h(t: string, ...c: (Record<string, any> | Vnode)[]): Vnode;
-export function render(current: Vnode, _currentNode?: Node, parentNode?: ParentNode | null, isSvg?: any, doc?: Document | null): boolean;
-export const elementFactory: any;
-export type SkruvChildNode = Vnode | Vnode[] | string | boolean | number;
-export type SkruvChildNodes = SkruvChildNode[];
 //# sourceMappingURL=index.d.ts.map
--- types/utils/jsx-runtime.d.ts
+++ types/utils/jsx-runtime.d.ts
@@ -1,10 +1,24 @@
 export const Fragment: "#fragment";
 export function jsx(nodeName: string, attributes?: JSXAttributes | undefined): Vnode | [Vnode?];
 export function jsxs(nodeName: string, attributes?: JSXAttributes | undefined): Vnode | [Vnode?];
-export type Vnode = typeof import("../index.js").Vnode;
+export type SkruvChildNode = Vnode | Vnode[] | string | boolean | number;
+export type SkruvChildNodes = SkruvChildNode[];
 export type VnodeAtrributes = Record<string, (string | boolean | Function | number | Object)>;
+export type Vnode = {
+    s: Symbol;
+    t: string;
+    c: SkruvChildNodes;
+    a: Record<string, (string | boolean | Function | number | Object)> & {
+        _r: {
+            _r: () => boolean;
+        } | null;
+    };
+    _r?: {
+        _r: () => boolean;
+    } | undefined;
+};
 export type JSXAdditionalProperties = {
-    children?: [import("../index.js").Vnode] | undefined;
+    children?: [Vnode] | undefined;
 };
 export type JSXAttributes = VnodeAtrributes & JSXAdditionalProperties;
 //# sourceMappingURL=jsx-runtime.d.ts.map
